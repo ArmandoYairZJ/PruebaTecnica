9820ebfc5841091543923ccf9138d2bd9cba88e7
Armando Zambrano <armando.zambrano@clickbalance>
2025-11-26 23:35:42 +0000

    fix(funcionales): Se corrigieron los endpoints para que funcionen de manera correcta, realicen la indicacion que tiene.

--- modules/domains/Usuarios/service.py
+++ modules/domains/Usuarios/service.py
@@ -1,6 +1,7 @@
 from modules.domains.Usuarios.models import user
 from modules.domains.Usuarios.schema import UserCreate
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 
 async def create_user(db: AsyncSession, data: UserCreate):
     userInstance = user(**data.model_dump())
@@ -10,23 +11,35 @@ async def create_user(db: AsyncSession, data: UserCreate):
     return userInstance
 
 async def get_user(db:AsyncSession):
-    return db.query(user).all()
+    result = await db.execute(select(user))
+    return result.scalars().all()
 
 async def get_user_id(db:AsyncSession, userId: int):
-    return db.query(user).filter(user.id == userId).first()
+    result = await db.execute(
+        select(user).where(user.id == userId)
+    )
+    return result.scalar_one_or_none()
 
 async def update_user(db:AsyncSession, userId:int, data:UserCreate):
-    userInstance = db.query(user).filter(user.id == userId).first()
-    if userInstance:
-        for key, value in user.model_dump().items():
-            setattr(userInstance, key, value)
-        await db.commit()
-        await db.refresh(userInstance)
+    result = await db.execute(
+        select(user).where(user.id == userId)
+    )
+    userInstance = result.scalar_one_or_none()
+    if not userInstance:
+        return None
+    for key, value in data.model_dump().items():
+        setattr(userInstance, key, value)
+    await db.commit()
+    await db.refresh(userInstance)
     return userInstance
 
 async def delete_user(db:AsyncSession, userId:int):
-    userInstance = db.query(user).filter(user.id == userId).first()
-    if userInstance:
-        await db.delete(userInstance)
-        await db.commit()
+    result = await db.execute(
+        select(user).where(user.id == userId)
+    )
+    userInstance = result.scalar_one_or_none()
+    if not userInstance:
+        return None
+    await db.delete(userInstance)
+    await db.commit()
     return userInstance
